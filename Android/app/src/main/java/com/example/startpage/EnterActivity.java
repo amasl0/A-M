d4f2f5ef09012dbf77e8e94f9b55205024d2c9d8
Masl0 <donssandman@gmail.com>
2020-07-04 12:43:32 +0300

    AddAct

---
+++ Android/app/src/main/java/com/example/startpage/EnterActivity.java
@@ -0,0 +1,15 @@
+package com.example.startpage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_page);
+    }
+}
