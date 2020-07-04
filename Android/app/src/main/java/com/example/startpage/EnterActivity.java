676e8deae9726d2ad3544530d867953ed3f60cf9
space1engine <space1engine@gmail.com>
2020-07-05 01:13:36 +0300

    Revert "1. Опять чутка переименовал файлы и методы"
    This reverts commit 7f4757a04c35bb02d0737b459892a0982dec9513.

--- Android/app/src/main/java/com/example/startpage/EnterActivity.java
+++ Android/app/src/main/java/com/example/startpage/EnterActivity.java
@@ -3,18 +3,14 @@ package com.example.startpage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EnterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.enter_activity);
-    }
-
-    public void onClickRegButton(View view) {
-        startActivity(new Intent(this, RegistrationActivity.class));
+        setContentView(R.layout.second_page);
     }
 }
 
--- Android/app/src/main/java/com/example/startpage/RegistrationActivity.java
+++ Android/app/src/main/java/com/example/startpage/RegistrationActivity.java
@@ -1,13 +1,17 @@
 package com.example.startpage;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
+
+
 
 public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration_activity);
+        setContentView(R.layout.registration);
     }
 }
